14d7d1d1553f0ecc262da72315476bfbba057ddf
info-1 <info-1@INFO1-PC5>
2025-03-19 20:53:49 -0300

    Agregar archivos de proyecto.

---
+++ Diagonistoc act1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagonistoc_act1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el numero correspondiente a la actividad que desee revisar");
+            int caso = int.Parse(Console.ReadLine());
+            switch (caso)
+            {
+
+                case 1:
+                    int Num = 0;
+                    while (Num <= 49)
+                    {
+                        Num++;
+                        Console.WriteLine(Num);
+                    }
+
+                    break;
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+
+    }
